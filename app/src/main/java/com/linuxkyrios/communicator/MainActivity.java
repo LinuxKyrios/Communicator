c62377e6d4860bf25c065db086412e593679976d
Linux Kyrios <malinowskim92@protonmail.com>
2020-11-08 20:26:06 +0100

    Created ReceiveMessageActivity with layout in activity_receive_message.xml for displaying message transmitted from first activity. Updated MainActivity with intent and getText() method nad putExtra() method.

--- app/src/main/java/com/linuxkyrios/communicator/MainActivity.java
+++ app/src/main/java/com/linuxkyrios/communicator/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -15,9 +16,14 @@ public class MainActivity extends Activity {
 
     //onSendMessage is performed when button is pressed
     public void onSendMessage(View view) {
+        //Downloading text saved in EditText component
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
         // Creating instance of intent class and assigning ReceiveMessageActivity as parameter,
         // then starting this activity
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        //This part of code creates intent and add  a text to it. Constant used to assign information
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/linuxkyrios/communicator/ReceiveMessageActivity.java
+++ app/src/main/java/com/linuxkyrios/communicator/ReceiveMessageActivity.java
@@ -1,14 +1,26 @@
 package com.linuxkyrios.communicator;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
 
 import android.os.Bundle;
 
-public class ReceiveMessageActivity extends AppCompatActivity {
+public class ReceiveMessageActivity extends Activity {
+    //Name of value transmitted in intent
+    public static final String EXTRA_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        //Two lines below are used to download intent and by
+        // getStringExtra method read delivered string
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView) findViewById(R.id.message);
+        // This line displays string in text view
+        messageView.setText(messageText);
     }
 }
